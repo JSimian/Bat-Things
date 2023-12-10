dd63d2b1b18dc6b5a49b3015f57d7728b97d370b
JSimian <14004210+JSimian@users.noreply.github.com>
2023-12-10 13:39:21 -0300

    Update Check if running as admin.bat
    Fixing an minor typo

--- Check if running as admin.bat
+++ Check if running as admin.bat
@@ -4,8 +4,9 @@ if %errorLevel% NEQ 0 (
   :: Personalize as you want the error message
   echo Error^!
   echo This script needs to run as administrator.
-  echo Press any key to exit and go back to Windfows explorer, right click the bat file
+  echo Press any key to exit and go back to Windows explorer, right click the bat file
   echo and click in 'Run as administrator'.
+
   pause >nul
   exit
 )
