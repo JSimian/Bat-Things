8634cf6af025eae0b6bcc15e225498f7139904d6
JSimian <14004210+JSimian@users.noreply.github.com>
2023-12-10 19:05:39 -0300

    Update Fix-AudioDG.bat

--- Fix-AudioDG.bat
+++ Fix-AudioDG.bat
@@ -4,7 +4,7 @@ net session >nul 2>&1
 if %errorLevel% NEQ 0 (
   echo Error^!
   echo This script needs to run as administrator.
-  echo Press any key to exit and go back to Windfows explorer, right click the bat file
+  echo Press any key to exit and go back to Windows explorer, right click the bat file
   echo and click in 'Run as administrator'.
   pause >nul
   exit
