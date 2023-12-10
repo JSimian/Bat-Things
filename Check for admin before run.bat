fc6308949d94231368e59e68254c47c466f0ae54
JSimian <14004210+JSimian@users.noreply.github.com>
2023-12-10 13:04:23 -0300

    Update Check for admin before run.bat

--- Check for admin before run.bat
+++ Check for admin before run.bat
@@ -1,12 +1,11 @@
 @echo off
 net session >nul 2>&1
-if %errorLevel% == 1 (
+if %errorLevel% NEQ 0 (
   :: Personalize as you want the error message
   echo Error^!
   echo This script needs to run as administrator.
-  echo Press any key to exit and go back to Windows explorer, right click the bat file
-  echo then click in 'Run as administrator'.
-
+  echo Press any key to exit and go back to Windfows explorer, right click the bat file
+  echo and click in 'Run as administrator'.
   pause >nul
   exit
 )
