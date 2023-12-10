c1be02768dd801b28a7e73db0be6ca4e674e596d
JSimian <14004210+JSimian@users.noreply.github.com>
2023-12-10 18:20:16 -0300

    Create Fix-AudioDG.bat

---
+++ Fix-AudioDG.bat
@@ -0,0 +1,16 @@
+@echo off
+Title Fix AudioDG sound Glitches
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+  echo Error^!
+  echo This script needs to run as administrator.
+  echo Press any key to exit and go back to Windfows explorer, right click the bat file
+  echo and click in 'Run as administrator'.
+  pause >nul
+  exit
+)
+ECHO Setting audiodg process affinity as single-core...
+powershell $process = Get-Process audiodg ; $process.ProcessorAffinity = 8
+ECHO Setting audiodg priority as high
+wmic process where name="audiodg.exe" call setpriority 32768
+exit
