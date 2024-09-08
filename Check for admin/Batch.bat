57c0a10a264c38babef8653a4703f5869de9da59
Simian <14004210+JSimian@users.noreply.github.com>
2024-09-08 13:14:13 -0300

    Update and rename Check if running as admin.bat to Check for admin/Batch.bat

--- Check for admin/Batch.bat
+++ Check for admin/Batch.bat
@@ -1,8 +1,8 @@
+::Source: https://github.com/JSimian/Bat-Things/
 @echo off
-::Source: https://github.com/JSimian/Bat-Things/blob/main/Fix-AudioDG.bat
 net session >nul 2>&1
 if %errorLevel% NEQ 0 (
-  :: Personalize as you want the error message
+  :: Personalize this message as you want the error message
   echo Error^!
   echo This script needs to run as administrator.
   echo Press any key to exit and go back to Windows explorer, right click the bat file
